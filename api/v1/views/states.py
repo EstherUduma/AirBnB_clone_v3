98a2e2f97213ef3902a0fd8d9ceaa14bd2a3eefd
Mmeso1 <anisiubammesoma8@gmail.com>
2024-03-30 07:41:47 -0700

    second part of task 6

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,7 +2,7 @@
 """
 view for State objects that handles all default RESTFul API actions
 """
-from flask import jsonify, abort, request, Blueprint
+from flask import jsonify, abort, request
 from api.v1.views import app_views
 from models import storage
 from models.state import State
@@ -12,3 +12,11 @@ from models.state import State
 def get_states():
     states = storage.all(State).values()
     return jsonify([state.to_dict() for state in states])
+
+@app_views.route('/states/<state_id>', methods=['GET'])
+def get_state_by_id(state_id):
+    states = storage.all(State).values()
+    for state in states:
+        if state.id == state_id:
+            return jsonify([state.to_dict()])
+    abort(404)
