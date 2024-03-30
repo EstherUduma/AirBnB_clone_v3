1f19ef9ca374abe72d5d9b4327454ca754a3d1a8
Esther Uduma <estheruduma23@gmail.com>
2024-03-30 09:33:00 -0700

    places

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""Initialize Blueprint views"""
+""" Blueprint for API """
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-# Import views
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
 from api.v1.views.index import *
 from api.v1.views.states import *
-from api.v1.views.cities import *
-from api.v1.views.amenities import  # Add this line to import the amenities view
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
+from api.v1.views.places_amenities import *
